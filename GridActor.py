e03212616def0f84f9eb801258c1682028788da2
Tim Horton <hortont424@gmail.com>
2009-12-20 15:07:01 -0500

    Grid color themes

--- GridActor.py
+++ GridActor.py
@@ -4,11 +4,28 @@ from OpenGL.GLU import *
 
 from Actor import *
 
+class GridTheme(object):
+    darkBackground = [0.0, 0.0, 0.0, 0.0]
+    lightBackground = [0.0, 0.0, 0.0, 0.0]
+    gridLines = [0.0, 0.0, 0.0, 0.0]
+
+class GridActorThemes(object):
+    green = GridTheme()
+    green.darkBackground = [0.000, 0.384, 0.145, 1.0]
+    green.lightBackground = [0.204, 0.643, 0.396, 1.0]
+    green.gridLines = [0.447, 0.812, 0.624, 0.8]
+    
+    blue = GridTheme()
+    blue.darkBackground = [0.000, 0.384, 0.145, 1.0]
+    blue.lightBackground = [0.204, 0.643, 0.396, 1.0]
+    blue.gridLines = [0.447, 0.812, 0.624, 0.8]
+
 class GridActor(Actor):
     def __init__(self):
         super(GridActor, self).__init__(0, 0)
         self.gridWidth = 15
         self.gridHeight = 15
+        self.theme = GridActorThemes().green
     
     def render(self):
         self.displayList = glGenLists(1)
@@ -17,16 +34,16 @@ class GridActor(Actor):
         glPushMatrix()
         
         glBegin(GL_QUADS)
-        glColor3f(0.000, 0.145, 0.384)
+        glColor4fv(self.theme.darkBackground)
         glVertex2f(0.0, 0.0)
         glVertex2f(self.window.width, 0.0)
-        glColor3f(0.204, 0.396, 0.643)
+        glColor4fv(self.theme.lightBackground)
         glVertex2f(self.window.width, self.window.height)
         glVertex2f(0, self.window.height)
         glEnd()
         
         glLineWidth(0.1)
-        glColor4f(0.447, 0.624, 0.812, 0.8)
+        glColor4fv(self.theme.gridLines)
 
         glBegin(GL_LINES)
         
